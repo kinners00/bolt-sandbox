941dd1f065932a046b3a7bab715bd98ac1936734
kinners00 <marcmckinley@me.com>
2021-02-28 23:44:50 +0000

    added sample content

---
+++ plans/myplan.pp
@@ -0,0 +1,10 @@
+plan bolt_sandbox::myplan(
+  TargetSpec $targets,
+  String $mytaskvar,
+) {
+
+  # upload_file($file_local, $file_dest, $targets)
+  # download_file($source_file, $local_file_dest, $win_targets)
+  # run_command('hostname', $targets, '_run_as' => 'root')
+  # run_task('bolt_sandbox::mytask', $targets, {'mytaskvar' => $mytaskvar })
+  # fail_plan("You must provide targets user name")
---
+++ tasks/mytask.sh
@@ -0,0 +1,3 @@
+
+
+echo $PT_mytaskvar
